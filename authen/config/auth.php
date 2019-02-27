c3f47f39dac27e29b6f962d20ddd7e2df217e47e
USER <nguyendangtruong300397@gmail.com>
2019-02-27 22:35:37 +0700

    123

--- authen/config/auth.php
+++ authen/config/auth.php
@@ -45,6 +45,15 @@ return [
             'driver' => 'token',
             'provider' => 'users',
         ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+        'admin-api' => [
+            'driver' => 'token',
+            'provider' => 'admins',
+        ]
+
     ],
 
     /*
@@ -69,6 +78,10 @@ return [
             'driver' => 'eloquent',
             'model' => App\User::class,
         ],
+        'admins' => [
+        'driver' => 'eloquent',
+        'model' => App\Model\AdminModel::class,
+        ]
 
         // 'users' => [
         //     'driver' => 'database',
@@ -97,6 +110,11 @@ return [
             'table' => 'password_resets',
             'expire' => 60,
         ],
+        'admins' => [
+            'provider' => 'admins',
+            'table' => 'password_resets',
+            'expire' => 15,
+        ]
     ],
 
 ];
--- authen/routes/web.php
+++ authen/routes/web.php
@@ -18,3 +18,56 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * * Route cho administrator
+ */
+Route::prefix('admin')->group(function (){
+    // Gom nhóm các route cho phần admin
+
+    /**
+     * url : authen.com/admin/
+     * Route mặc định của admin
+     */
+   Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
+    /**
+     * URL : authen.com/admin/dashboard
+     * Route đăng nhập thành công
+     */
+   Route::get('/dashboard', 'AdminController@index')->name('admin.dashboard');
+
+   /**
+    * URL : authen.com/admin/register
+    * Route trả về view dùng dể đăng ký tài khoản admin
+    */
+   Route::get('register', 'AdminController@create')->name('admin.register');
+
+    /**
+     * URL : authen.com/admin/register
+     * Phương thức là POST
+     * Route dùng để đăng ký 1 admin từ form POST
+     */
+    Route::post('register', 'AdminController@store')->name('admin.register.store');
+
+    /**
+     * URL : authen.com/admin/login
+     * METHOD : GET
+     * Route trả về view đăng nhập admin
+     */
+    Route::get('login', 'Auth\Admin\LoginController@login')->name('admin.auth.login');
+
+    /**
+     * URL : authen.com/admin/login
+     * METHOD : POST
+     * Route xử lý quá trình đăng nhập admin
+     */
+    Route::post('login', 'Auth\Admin\LoginController@loginAdmin')->name('admin.auth.loginAdmin');
+
+    /**
+     * URL : authen.com/admin/logout
+     * METHOD : POST
+     * Route dùng để đăng xuất
+     */
+    Route::post('logout', 'Auth\Admin\LoginController@logout')->name('admin.auth.logout');
+});
